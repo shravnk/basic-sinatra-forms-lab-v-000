247475c8f57e13f4f5af534798dc23f5197222f5
Brendan O'Dea <brendan.ode4@gmail.com>
2018-03-20 23:23:03 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,9 +1,12 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
-
+  get '/newteam' do
+    erb :newteam
+  end
   post '/newteam' do
     @team_name = params['team_name']
+    erb :team
   end
 
 end
